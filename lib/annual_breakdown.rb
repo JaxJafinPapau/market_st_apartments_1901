6e8b141f28f205e4013e169fc1ae61932add9b1c
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-06 10:02:00 -0700

    Initial commit annual_breakdown and test

---
+++ lib/annual_breakdown.rb
@@ -0,0 +1,4 @@
+require 'pry'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
---
+++ test/annual_breakdown_test.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+require './lib/annual_breakdown'
+
+class AnnualBreakdownTest < Minitest::Test
+  
+end
