c315f6d5dc0ad52dce32464f5343b01e3e53fd27
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-06 09:40:02 -0700

    Adds average_rent method and test

--- lib/building.rb
+++ lib/building.rb
@@ -3,6 +3,20 @@ require './lib/apartment'
 require './lib/renter'
 
 class Building
+  attr_reader :units
 
+  def initialize
+    @units = []
+  end
 
+  def add_unit(apartment)
+    @units << apartment
+  end
+
+  def average_rent
+    total = @units.map do |unit|
+      unit.monthly_rent
+    end
+    total.sum.to_f / @units.count
+  end
 end
---
+++ test/building_test.rb
@@ -0,0 +1,44 @@
+require 'pry'
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/apartment'
+require './lib/renter'
+require './lib/building'
+
+class BuildingTest < Minitest::Test
+
+  def setup
+    @jessie = Renter.new("Jessie")
+    @spencer = Renter.new("Spencer")
+    @a1 = Apartment.new({
+      number: "A1", monthly_rent: 1200,
+      bathrooms: 1, bedrooms: 1
+      })
+    @b2 = Apartment.new({
+      number: "B2", monthly_rent: 999,
+      bathrooms: 2, bedrooms: 2
+      })
+    @building = Building.new
+  end
+
+  def test_building_exists
+    assert_instance_of Building, @building
+  end
+
+  def test_building_starts_empty
+    assert_equal [], @building.units
+  end
+
+  def test_building_can_add_units
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+
+    assert_equal [@a1, @b2], @building.units
+  end
+
+  def test_building_can_calculate_average_rent
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+    assert_equal 1099.5, @building.average_rent
+  end
+end
